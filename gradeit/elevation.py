bd4d053a2bcf3334aded22055ea540da52559bdd
Cory Kennedy <corykennedy@mines.edu>
2019-03-02 16:16:53 -0700

    added elevation.usgs_api_elevation(), elevation.get_elevation() now calls this by default. Units are in feet. Tested proper api return values.

--- gradeit/elevation.py
+++ gradeit/elevation.py
@@ -1,7 +1,7 @@
 """Module contains functionality associated with elevation profiles."""
 
-# NOTE: temporary for functon i/o testing
-from numpy.random import randint
+import requests
+from json import loads
 
 def get_elevation(coordinates):
     """
@@ -31,14 +31,60 @@ def get_elevation(coordinates):
 
     elevations = []
 
-    # get each elevation value
+    # get each elevation value from USGS API
     for coord in coordinates:
 
-        # TODO: make this an actual elevation value from the database
-        elev = float(randint(100, 100000)) / 100
-        # append it to the elevations list
+        # query USGS API and store resulting elevation
+        elev = usgs_api_elevation(coord)
+        # append elevation to the elevations list
         elevations += [elev]
 
     return tuple(elevations)
 
 
+def usgs_api_elevation(coordinate):
+    """
+    A function that queries the USGS DEM API (https://nationalmap.gov/epqs/) and
+    returns the elevation value at the provided latitude/longitude coordinate.
+
+    Parameters:
+        List/Tuple that contains latitude and longitude floating-point
+        values. These two values comprise a coordinate
+
+        ex:
+            Tuple of floats
+            (xx.xxxxxx, xxx.xxxxxx)
+
+            or
+
+            List of floats
+            [lat_1, lon_1]
+
+    Returns:
+        A float containing a floating-point elevation value.
+    """
+    # TODO: handle invalid elevation values (-1000000)
+
+    URL = 'https://nationalmap.gov/epqs/pqs.php'
+    lat = str(coordinate[0])
+    lon = str(coordinate[1])
+    UNITS = 'feet'
+    OUTPUT = 'json'
+
+    query = '{url}?x={lon}&y={lat}&units={units}&output={output}'.format(
+            url=URL, lon=lon, lat=lat, units=UNITS, output=OUTPUT
+            )
+
+    response_json = requests.get(query)
+
+    results = loads(response_json.text) # a dict containing the json data
+
+
+    elev = results['USGS_Elevation_Point_Query_Service']\
+                    ['Elevation_Query']\
+                    ['Elevation']
+
+    # cast value to a float
+    elev = float(elev)
+
+    return elev
