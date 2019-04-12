40603977b0a7e12cc9ef2e942d47bded9c7816c4
Cory Kennedy <ckennedy@arnaud.hpc.nrel.gov>
2019-04-12 12:36:09 -0600

    pushed elevation source option functionality up to gradeit module, gradeit.gradeit() now takes a source keyword argument and can get elev from either arnaud raster db or usgs api

--- gradeit/gradeit.py
+++ gradeit/gradeit.py
@@ -3,43 +3,59 @@
 import elevation
 import grade
 
-def gradeit(coordinates):
+def gradeit(coordinates, source='usgs-api'):
     """
     A function that provides elevation and road grade values given coordinates
 
     Parameters:
-        nested lists/tuples that contain latitude and longitude floating-point
-        coordinates.
+	coordinates:
+	    nested lists/tuples that contain latitude and longitude floating-point
+            coordinates.
 
-        ex:
-            Tuple of tuples of floats
-            ((xx.xxxxxx, xxx.xxxxxx), (xx.xxxxxx, xxx.xxxxxx))
+            ex: Tuple of tuples of floats
+                ((xx.xxxxxx, xxx.xxxxxx), (xx.xxxxxx, xxx.xxxxxx))
 
-            or
+                or
 
-            List of lists of floats
-            [[lat_1, lon_1], [lat_2, lon_2]]
+                List of lists of floats
+                [[lat_1, lon_1], [lat_2, lon_2]]
+
+	source:
+	    valid keywords are:
+		
+		'usgs-api' (default):
+		    sources elevation from a point-query API hosted by USGS
+		or
+		'arnuad-server':
+		    sources elevation values from a raster database stored on arnaud
+
+		NOTES: 	- Both sources are technically the same USGS 1/3 arc-second dataset.
+			- However, slight variations in returned elevation values are likely.
+			- Furthermore, there are performance differences between the sources.
+			- Generally, we recommend selecting the USGS API for smaller queries
+			  and arnaud for larger queries.
 
     Returns:
-        A dictionary containing string keys and tuple values
-        where the keys are 'elevation' and 'grade' and the values contain
-        floating-point elevation and grade values.
+	A dictionary containing elevation, grade, and source information.
 
-        ex:
-            grade_dictionary = {
-                            'elevation' : (elev_val_1, elev_val_..., elev_val_n)
-                            'grade' : (grade_val_1, grade_val_..., grade_val_n)
-                            }
+	ex:
+	    grade_dictionary = {
+			    'elevation' : (elev_val_1, elev_val_..., elev_val_n),
+			    'grade' : (grade_val_1, grade_val_..., grade_val_n),
+			    'source' : 'source name'
+			    }
     """
     # get elevation values
-    elev_tuple = elevation.get_elevation(coordinates)
+    elev_tuple = elevation.get_elevation(coordinates, source=source)
+     
     # get grade values
     grade_tuple = grade.get_grade(coordinates)
 
     # place both tuples in a dictionary
     gradeit_dict = {
-                    'elevation':elev_tuple,
-                    'grade':grade_tuple
+                    'elevation' : elev_tuple,
+                    'grade' : grade_tuple,
+		    'source' : source,
                     }
 
     return gradeit_dict
