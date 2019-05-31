bce5fd61a4f6a4d43669adf439fecb2697dac39f
Holden, Jacob <Jacob.Holden@nrel.gov>
2019-05-31 13:36:11 -0600

    grade_tests complete

--- tests/grade_tests.py
+++ tests/grade_tests.py
@@ -13,7 +13,12 @@ class GradeTest(unittest.TestCase):
         self.data['lat'] = np.linspace(39.702730, 39.695368, 10)
         self.data['lon'] = np.linspace(-105.245678, -105.209049, 10)
     
+        self.data['elev_ft'] = [7051.126700732422, 7019.6391271875, 7166.562447382812, 6999.898018144531, 6919.229083066406, 6827.498526796875, 6672.75637855957, 6740.378447832031, 6551.383320751953, 6451.975711020507]
     
+        self.distance = [1336.0892816, 1336.0892816, 1336.0892816, 1336.0892816, 1336.12209, 1336.12209, 1336.12209, 1336.12209, 1336.1548984]
+
+        self.data['grade'] = [0.0, -0.0236, 0.11, -0.1247, -0.0604, -0.0687, -0.1158, 0.0506, -0.1415, -0.0744]
+
     def test_haversine_no_bearing(self):
                 
         dist_km = grade.haversine(self.data.lat[0], 
@@ -39,13 +44,14 @@ class GradeTest(unittest.TestCase):
         coords = list(zip(self.data.lat, self.data.lon))
         dist_ft = grade.get_distances(coords)
         
-        i70_expected = [1336.0892816, 1336.0892816, 1336.0892816, 1336.0892816, 1336.12209, 1336.12209, 1336.12209, 1336.12209, 1336.1548984]
+        self.assertEqual(dist_ft, self.distance)
+    
+    
+    def test_get_grade(self):
         
-        self.assertEqual(dist_ft, i70_expected)
+        dist_out, grade_out = grade.get_grade(self.data.elev_ft, distances=self.distance)
     
-    
-    def test_get_grade()
-    
+        self.assertEqual(list(grade_out), list(self.data.grade))
             
 if __name__ == '__main__':
     unittest.main(warnings='ignore') 
