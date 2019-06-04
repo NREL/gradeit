89542804eb9f795f7b33593b76555b9b7a7e69bf
jholden <jacob.holden@nrel.gov>
2019-06-04 10:19:44 -0600

    gradeit.py passing non-filtering tests

--- tests/gradeit_tests.py
+++ tests/gradeit_tests.py
@@ -23,11 +23,15 @@ class GradeitTest(unittest.TestCase):
         self.data['lat'] = np.linspace(39.702730, 39.695368, 10)
         self.data['lon'] = np.linspace(-105.245678, -105.209049, 10)
 
-        self.check_usgs_df = pd.DataFrame()
         self.check_usgs_df = self.data.copy()
         self.check_usgs_df['elevation_ft'] = [7048.15, 7015.69, 7157.89, 7004.84, 6921.27, 6840.03, 6696.7, 6735.26, 6554.42, 6445.5]
         self.check_usgs_df['distance_ft'] = [0, 1336.0892816, 1336.0892816, 1336.0892816, 1336.0892816, 1336.12209, 1336.12209, 1336.12209, 1336.12209, 1336.1548984]
         self.check_usgs_df['grade_dec'] = [0.0, -0.0243, 0.1064, -0.1146, -0.0625, -0.0608, -0.1073, 0.0289, -0.1353, -0.0815]
+
+        self.check_raster_df = self.data.copy()
+        self.check_raster_df['elevation_ft'] = [7051.12670073, 7019.63912719, 7166.56244738, 6999.89801814, 6919.22908307, 6827.4985268, 6672.75637856, 6740.37844783, 6551.38332075, 6451.97571102]
+        self.check_raster_df['distance_ft'] = [0., 1336.0892816, 1336.0892816, 1336.0892816, 1336.0892816, 1336.12209, 1336.12209, 1336.12209, 1336.12209, 1336.1548984]
+        self.check_raster_df['grade_dec'] = [0., -0.0236, 0.11, -0.1247, -0.0604, -0.0687, -0.1158, 0.0506, -0.1415, -0.0744]
 
     def tearDown(self):
         pass
@@ -50,10 +54,8 @@ class GradeitTest(unittest.TestCase):
                         lon_col = 'lon',
                         filtering = False,
                         source = 'arnaud-server')
-
-        print(self.data)
-        
-        # pd.testing.assert_frame_equal(self.data, self.check_usgs_df)
+       
+        pd.testing.assert_frame_equal(self.data, self.check_raster_df)
         
 
 if __name__ == '__main__':
