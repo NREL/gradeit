782a90860d833d0148a7696b24113bb5355d8245
Jake Holden <jacob.holden@nrel.gov>
2020-06-03 12:53:59 -0600

    removed unnecessary environment files and added setup docs to README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # GradeIT
-Road Grade Inference Tool (a.k.a. GradeIT a.k.a. GRIT) - a python package to append elevation and road grade to a sequence of GPS points.
+Road Grade Inference Tool (GradeIT) - a python package to append elevation and road grade to a sequence of GPS points.
+
+## Overview
+
+## Setup
+Clone or download the git repository and re-create the conda environment to automatically ensure that python and package 
+version dependencies are satisfied.
+
+```git clone https://github.com/NREL/gradeit.git```
+
+```cd gradeit```
+
+If you do not have the conda package manager, you can download and install it from the 
+[Anaconda website](https://www.anaconda.com/). Then conda will create a new environment from the ```environment.yml``` 
+file in this repository.
+
+```conda env create -f environment.yml```
+
+```conda activate gradeit```
+
+You have now created and activated the gradeit conda environment and are ready to run the package.
--- tests/basic.py
+++ tests/basic.py
@@ -27,17 +27,17 @@ data['lon'] = np.linspace(-105.245678, -105.109049, 50)
 #                                           lat='lat',
 #                                           lon='lon',
 #                                           filter=False)
-# t1 = time.time()
-# df_api = gradeit(df=data, lat_col='lat', lon_col='lon', filtering=False, source='usgs-api')
-# t2 = time.time()
-# print('API time: ', t2-t1)
+t1 = time.time()
+df_api = gradeit(df=data, lat_col='lat', lon_col='lon', filtering=False, source='usgs-api')
+t2 = time.time()
+print('API time: ', t2-t1)
 
-t3 = time.time()
-df_local = gradeit(df=data, lat_col='lat', lon_col='lon', filtering=False, source='usgs-local',
-                   usgs_db_path="/Volumes/ssh/backup/mbap_shared/NED_13/")
-t4 = time.time()
+# t3 = time.time()
+# df_local = gradeit(df=data, lat_col='lat', lon_col='lon', filtering=False, source='usgs-local',
+#                    usgs_db_path="/Volumes/ssh/backup/mbap_shared/NED_13/")
+# t4 = time.time()
 
-print('Local time: ', t4-t3)
+# print('Local time: ', t4-t3)
 # print(output)
 # print(output_fltr)
-print(df_local)
+print(df_api)
